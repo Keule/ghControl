a6a96e31b0c44d002f15ea0cda8163f6795d335d
Thomas Wolters <tw@fluidmobile.de>
2015-05-16 22:21:31 +0200

    initial commit

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -12,7 +12,7 @@ int led = 2;
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
-  pinMode(led, OUTPUT);     
+  pinMode(led, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
