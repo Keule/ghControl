842f47f56f11bcfb2fc051c339f39e82f0f8aaaa
Thomas Wolters <tw@fluidmobile.de>
2015-05-17 19:30:31 +0200

    added commandSetActorValue, commandValueList, added watchdog

--- arduinoUno/arduinoUno.ino
+++ arduinoUno/arduinoUno.ino
@@ -2,9 +2,17 @@ const int COMMAND_SENSOR_LIST = 1;
 const int COMMAND_ACTOR_LIST = 3;
 const int COMMAND_ACTOR_SET_VALUE = 5;
 
+const int ID_SENSOR_HUMIDITY_DIGITAL = 0;
+const int ID_SENSOR_HUMIDITY_ANALOG = 1;
+
 const int SENSOR_TYPE_HUMIDITY_ANALOG = 0;
 const int SENSOR_TYPE_HUMIDITY_DIGITAL = 1;
 
+const int ID_ACTOR_LED = 0;
+
+const int ACTOR_TYPE_LED = 0;
+
+#include <avr/wdt.h>
 
 int led = 2;
 int humidAnalog = 0;
@@ -18,28 +26,23 @@ void setup() {
   pinMode(led, OUTPUT);
   pinMode(humidDigital, INPUT);  
   Serial.begin(9600);  
+  wdt_enable(WDTO_8S);
 }
 
 void loop() {
-/*  
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);               // wait for a second
-  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)  
+//TOBI
+/*
+executeCommandListSensor();
   delay(1000);   
-*/
-
-//  int valDigital = digitalRead(humidDigital);   // read the input pin
-//  Serial.print(valDigital,DEC);
-
-//  float valAnalog = analogRead(humidAnalog);    // read the input pin
-//  Serial.print(valAnalog,DEC);
-
+  */
+//  
 
   if (Serial.available() > 0){
     String input = loadInputStream();
     if (input.length() > 0){
-       int commandID =  commandIDForString(input);
-      executeCommandID(commandID, input);       
+       int commandID =  commandIDForString(input,0,1,2);
+        executeCommandID(commandID, input); 
+        inputString = "";
     }
   }
 }
@@ -78,14 +81,21 @@ void printCommandBufferUnderflow(){
   printCommandBufferOverflow();
 }
 
+void printActorNotFound(){
+  printCommandBufferOverflow();  
+}
+
 void executeCommandID(int commandID, String information){
-          Serial.println ("---EXECUTE---");          
-          Serial.write ("String: ");          
-          Serial.print(inputString); 
-          Serial.write ("command: ");          
-          Serial.print(commandID);           
-        
-          
+  wdt_reset();  
+  Serial.print ("---EXECUTE---");          
+  newline();          
+
+  Serial.write ("COMMAND: ");          
+  Serial.print(commandID);           
+  Serial.write (" (complete: ");          
+  Serial.print(inputString);         
+  Serial.write (")");
+  
   if (commandID == COMMAND_SENSOR_LIST){
       executeCommandListSensor();
   }
@@ -93,56 +103,84 @@ void executeCommandID(int commandID, String information){
       executeCommandListActor();    
   }
   else if (commandID == COMMAND_ACTOR_SET_VALUE){
-    
-  }  
+    int actorID = commandIDForString(information,4,5,6);
+    int value = commandIDForString(information,8,9,10);
+     executeCommandSetActor(actorID, value);
+  }   
 }
 
 void executeCommandListSensor(){
-  Serial.println(COMMAND_SENSOR_LIST+1);
+  Serial.print(COMMAND_SENSOR_LIST+1);
+  newline();  
    
   int valDigital = digitalRead(humidDigital);   // read the input pin
   int valAnalog = analogRead(humidAnalog);    // read the input pin
    
-  printSensorLine(0, valDigital, SENSOR_TYPE_HUMIDITY_DIGITAL, 1);
-  printSensorLine(1, valAnalog, SENSOR_TYPE_HUMIDITY_ANALOG, 1);  
+  printInformationLine(ID_SENSOR_HUMIDITY_DIGITAL, valDigital, SENSOR_TYPE_HUMIDITY_DIGITAL, 1);
+  printInformationLine(ID_SENSOR_HUMIDITY_ANALOG, valAnalog, SENSOR_TYPE_HUMIDITY_ANALOG, 1);  
      
   Serial.write(eof);
 }
 
-void printSensorLine(int sensorID, int sensorValue, int sensorType, int sensorStatus){
-    Serial.print(sensorID);
+void printInformationLine(int id, int value, int type, int status){
+    Serial.print(id);
     Serial.write(",");
-    Serial.print(sensorType);
+    Serial.print(type);
     Serial.write(",");
-    Serial.print(sensorValue,DEC);
+    Serial.print(value,DEC);
     Serial.write(",");
-    Serial.print(sensorStatus);  
-    Serial.println(); 
+    Serial.print(status);  
+    newline();
 }
 
 void executeCommandListActor(){
-   Serial.println(COMMAND_ACTOR_LIST+1);  
-   Serial.write(eof);   
+  Serial.print(COMMAND_ACTOR_LIST+1);  
+  newline();  
+  printInformationLine(ID_ACTOR_LED, digitalRead(led), ACTOR_TYPE_LED, 1);
+  Serial.write(eof);   
 }
 
 void executeCommandSetActor(int actorID, int value){
-   Serial.println(COMMAND_ACTOR_SET_VALUE+1);  
-   Serial.write(eof);   
-}
-
-void writeStringOnSerial(String string){
-  char* buf;
-  string.toCharArray(buf, string.length()+1); // **CRASH** buf is not allocated!
- // Serial.write(buf,string.length);
+  if (actorID == ID_ACTOR_LED){
+     if (value == 1){
+      digitalWrite(led, HIGH);  
+     }      
+     else{
+      digitalWrite(led, LOW);          
+     }
+  }
+  else{
+    printActorNotFound();
+    return;
+  }
+    
+  Serial.print(COMMAND_ACTOR_SET_VALUE+1);  
+  newline();
+  Serial.write(eof);   
 }
 
 int intForChars(int char0, int char1, int char2){
     return char0*100 + char1*10 + char2 *1;
 }
   
-int commandIDForString(String string){
-  int char0 =  string.charAt(0) - '0';
-  int char1 =  string.charAt(1) - '0';
-  int char2 =  string.charAt(2) - '0';  
+int commandIDForString(String string, int position0, int position1, int position2){
+  int char0 =  string.charAt(position0) - '0';
+  int char1 =  string.charAt(position1) - '0';
+  int char2 =  string.charAt(position2) - '0';  
   return intForChars(char0, char1, char2);
 }  
+
+void blinkLED(){
+  digitalWrite(led, HIGH);   
+  delay(1);              
+  digitalWrite(led, LOW); 
+  delay(40);              
+  digitalWrite(led, HIGH);
+  delay(1);               
+  digitalWrite(led, LOW); 
+  delay(3000);   
+}  
+
+void newline(){
+   Serial.write("\n"); 
+}
